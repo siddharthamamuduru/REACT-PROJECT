592d264e3daf8047661dae1b1510ae16764b6c90
siddharth <siddumamuduru987@gmail.com>
2025-08-16 00:16:25 +0530

    react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = ({theme}) => {
     transition={{ duration: 0.5, delay: 0.4 }}
     viewport={{ once: true }}
       className='pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap'>
-        <p>Copyright 2025 © GreatStack - All Right Reserved.</p>
+        <p>Copyright 2025 @ SIDDHARTHA MAMUDURU.</p>
         <div className='flex items-center justify-between gap-4'>
             <img src={assets.facebook_icon} alt="" />
             <img src={assets.twitter_icon} alt="" />
